2d190b81ef511f516d2a37cb7670112f6a53614f
Shubha Agrawal <shubh.cap@gmail.com>
2025-01-02 16:09:04 -0500

    Auto Version 1 -Lotus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOTUSAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOTUSAuto.java
@@ -1,4 +1,82 @@
-package org.firstinspires.ftc.teamcode;
 
-public class LOTUSAuto {
+package org.firstinspires.ftc.teamcode.teleops;
+
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Config
+@Autonomous(name = "LOTUSAuto", group = "Autonomous")
+public class LOTUSAuto extends LinearOpMode {
+    double startPosition;
+
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(-24, 72, Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .lineToX(-72);
+
+//                .lineToYSplineHeading(33, Math.toRadians(0))
+//                .waitSeconds(2)
+//                .setTangent(Math.toRadians(90))
+//                .lineToY(48)
+//                .setTangent(Math.toRadians(0))
+//                .lineToX(32)
+//                .strafeTo(new Vector2d(44.5, 30))
+//                .turn(Math.toRadians(180))
+//                .lineToX(47.5)
+//                .waitSeconds(3);
+        TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
+                .lineToY(37)
+                .setTangent(Math.toRadians(0))
+                .lineToX(18)
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(0))
+                .lineToXSplineHeading(46, Math.toRadians(180))
+                .waitSeconds(3);
+
+        TrajectoryActionBuilder tab3 = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(180))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(46, 30))
+                .waitSeconds(3);
+
+        Action trajectoryActionCloseOut = tab1.endTrajectory().fresh()
+                .strafeTo(new Vector2d(-72, 72))
+                .build();
+
+        if (isStopRequested())
+            return;
+        Action trajectoryActionChosen;
+        if (startPosition == 1) {
+            trajectoryActionChosen = tab1.build();
+        } else if (startPosition == 2) {
+            trajectoryActionChosen = tab2.build();
+        } else {
+            trajectoryActionChosen = tab3.build();
+        }
+        Actions.runBlocking(new SequentialAction(
+                trajectoryActionChosen,
+                trajectoryActionCloseOut));
+
+    }
 }
